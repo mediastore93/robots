ac34781813357209ecf6a1ec502b27f438162af6
mediastore93 <mediastore93@gmail.com>
2018-05-23 19:52:30 -0700

    Update robby_keys2.py

--- robby_keys2.py
+++ robby_keys2.py
@@ -14,28 +14,37 @@ def right():
 def stop():
     robby.stop()
 
-while True: 
+try: 
 
     direction = input("Which direction - f, b, l, r?")
     time.sleep(1)
-    distance = input("Drive for ? seconds?")
+    distance = int(input("Drive for ? seconds?"))
 
-        if direction == "f":
-            forward()
-            time.sleep(distance)
-            stop()
-        if direction == "b":
-            backward()
-            time.sleep(distance)
-            stop()
-        if direction == "l":
-            left()
-            time.sleep(distance)
-            stop()
-        if direction == "r":
-            right()
-            time.sleep(distance)
-            stop()
+    if direction == "f":
+        forward()
+        time.sleep(distance)
+        stop()
+        time.sleep(1)
+    if direction == "b":
+        backward()
+        time.sleep(distance)
+        stop()
+        time.sleep(1)
+    if direction == "l":
+        left()
+        time.sleep(distance)
+        stop()
+        time.sleep(1)
+    if direction == "r":
+        right()
+        time.sleep(distance)
+        stop()
+        time.sleep(1)
+
+except KeyboardInterrupt:
+       pass
+#finally:
+#    GPIO.cleanup()
 
 
 
